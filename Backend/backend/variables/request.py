85a4817801f3974ad0e8a01ed1e89c12d2ea5722
francoisPgbn <francoispgbn@gmail.com>
2022-06-22 19:37:54 +0200

    add  request const

---
+++ Backend/backend/variables/request.py
@@ -0,0 +1,56 @@
+COOKIES = {
+    'intercom-id-nugfwmf7': '90814afb-c2f0-4f83-a48b-86fa7080419e',
+    'SAAGIETOKENDEMO': 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJxM0JuaTVIUjVMc3ZUblZvUzZKQ3k4d1oxMEhHY0g1X3BZclN0TGwyTnpBIn0.eyJqdGkiOiJmMmMzOTlhMS0wNDc2LTQwZTktYjA4ZC0zYzM0MzRlMzlkM2QiLCJleHAiOjE2NTU5NTA2NDQsIm5iZiI6MCwiaWF0IjoxNjU1OTA3NDQ0LCJpc3MiOiJodHRwOi8vc2FhZ2llLWNvbW1vbi1rZXljbG9hazo4MDgwL2F1dGgvcmVhbG1zL2RlbW8iLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiN2M5Y2JkZTctY2FiNy00ZGQ1LTk4MTYtZTQ0ZTcxMTVhNDMxIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiZnJvbnRlbmQiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiI4MzZiZjlhOC1jOGUyLTQ2MDQtOTkwMC1jYTlkNWU4ODU4N2IiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJyZWFsbS1tYW5hZ2VtZW50Ijp7InJvbGVzIjpbIm1hbmFnZS11c2VycyIsIm1hbmFnZS1jbGllbnRzIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJlc3RpYW1fZzE0X2VsaWUtZGVuaXMuZGVjaGFzc2UgZXN0aWFtX2cxNF9lbGllLWRlbmlzLmRlY2hhc3NlIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiZXN0aWFtX2cxNF9lbGllLWRlbmlzLmRlY2hhc3NlIiwiZ2l2ZW5fbmFtZSI6ImVzdGlhbV9nMTRfZWxpZS1kZW5pcy5kZWNoYXNzZSIsImZhbWlseV9uYW1lIjoiZXN0aWFtX2cxNF9lbGllLWRlbmlzLmRlY2hhc3NlIiwiZW1haWwiOiJlc3RpYW1fZzE0X2VsaWUtZGVuaXMuZGVjaGFzc2VAZGVtby5hNC5zYWFnaWUuaW8ifQ.N3FDptAu6Po4m4apSs-5W9-O-X__ckXuKS07hRxrhE5wK0BgxIHKGcPt7NeSluQ3GD0KWrH2yT9hWSfi3ZOYZKjjw5mX7VkgPPrzZr4mK3ezQowaLGsupNzTNnD-fj7HoKX5MwJ7xMaoiQyeXGmlLy-iUt52A7f9qcQKYyNY3anYOjCk2CF6SVrHOa1XiPjzjryAt-IF_jf905xD8dN13ihCS0iS9PTWfTllZf-Ux-6TeakCVR-SO06BNL21BfIWhqf9kaR-8oBIRu3QbU5rbIj56GyuKr19qN1y5BIayHXgDN3SPXpl9l_lyCBMvlik6s-l8RZCGEiMLsPufzXm8w',
+    'intercom-session-nugfwmf7': 'WWNyZ3hJVjJqdjlTcjRTY1BOdWJ1anBoOUtvQktNY3BGMXJnSXl0ZXBlYk5TMFExN3gwaGpDc2tiMjRHK09nVS0tN2Z4TlZyYTNuUmxKbTBNZVRtTExlQT09--5618312bc869e97e5fa566265deb6e9f8f49ce47',
+}
+
+
+HEADERS_TECHNO = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0',
+    'Accept': '*/*',
+    'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Referer': 'https://demo-workspace.a4.saagie.io/gateway/api/graphiql',
+    'Origin': 'https://demo-workspace.a4.saagie.io',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+}
+
+
+HEADERS_PROJECT = {
+    'Accept': 'application/json',
+    'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Connection': 'keep-alive',
+    'Origin': 'https://demo-workspace.a4.saagie.io',
+    'Referer': 'https://demo-workspace.a4.saagie.io/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+
+def getJsonDataProject(id):
+    return {
+        'query': '\n{\n  job(id: "'+id+'") {\n    id\n    name\n    versions {\n      number\n      packageInfo {\n        name\n        downloadUrl\n      }\n    }\n  }\n}',
+        'variables': None,
+    }
+
+def getJsonDataInfo(id):
+    return {
+    'operationName': None,
+    'variables': {},
+    'query': '{\n  technology(id: "'+id+'") {\n    available\n    label\n    id\n    description\n  }\n}\n',
+}
+
+
+URL_PROJECT = 'https://demo-workspace.a4.saagie.io/projects/api/platform/2/graphql'
+
+URL_TECHNO = 'https://demo-workspace.a4.saagie.io/gateway/api/graphiql'
+
+# Exemple utilisation
+# response = requests.post(URL_TECHNO, cookies=COOKIES, headers=HEADERS_PROJECT, json=getJsonDataProject(id))

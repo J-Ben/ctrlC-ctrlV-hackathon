6917ac3940b21bbd0eba7b3e97ba09a32df075c7
landryEstiam <kokou-kami-landry-ulrich.maman@estiam.com>
2022-06-22 11:39:23 +0200

    Ajout de la modal

--- front/app.js
+++ front/app.js
@@ -1,6 +1,3 @@
-
-
-
 function getList() {
 
     var myHeaders = new Headers();
@@ -10,7 +7,7 @@ var myInit = { method: 'GET',
                mode: 'no-cors',
                cache: 'no-cache' };
 
-    fetch('http://127.0.0.1:5000/test',myInit)
+    fetch('./assets/data/projects.json',myInit)
     .then(response => {
         
         console.log(response)
@@ -18,8 +15,6 @@ var myInit = { method: 'GET',
    
 
 }
-
-
 
 function render() {
     console.log('click')
@@ -33,6 +28,7 @@ function render() {
     var id = $(this).data('id'); 
    var dataId = $(this).attr("data-id");
    console.log("The data-id of clicked item is: " + dataId + " "+ id);
+   $("#ctrl-modal-audit").addClass("is-active"); 
  });
     
 }

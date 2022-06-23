d5907003ed520cf6340d1492ba8295f1e9b27a61
StigmaED <92782434+StigmaED@users.noreply.github.com>
2022-06-23 10:48:32 +0200

    Add files via upload

--- Backend/backend/main.py
+++ Backend/backend/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 import variables.saagie as saagie
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -10,9 +12,20 @@ def index():
 
 @app.route('/test')
 def a():
-    projects = saagie.API.projects.list()
+    projects = saagie.API.projects.list(id)
     return projects
     
+@app.route('/test/<string:idproject>')
+def pinfo(idproject):
+    
+    info = saagie.API.jobs.get_info(idproject)
+    return info
+
+@app.route('/testjob/<string:idjob>')
+def jinfo(idjob):
+    
+    infojob = saagie.API.jobs.get_info(idjob)
+    return infojob
 
 
 if __name__ == "__main__":

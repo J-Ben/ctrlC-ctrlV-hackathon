09707d4931334f980c7c1a7c3c43615994f6fb00
StigmaED <92782434+StigmaED@users.noreply.github.com>
2022-06-23 15:13:16 +0200

    Add files via upload

--- Backend/backend/main.py
+++ Backend/backend/main.py
@@ -10,22 +10,50 @@ def index():
     return "Hello world !!!!!"
 
 
-@app.route('/test')
-def a():
-    projects = saagie.API.projects.list(id)
-    return projects
-    
 @app.route('/test/<string:idproject>')
-def pinfo(idproject):
-    
-    info = saagie.API.jobs.get_info(idproject)
-    return info
+def a():
+    projects = saagie.API.projects.list()
+    for  project in projects['projects']:
+        projectId = project['id']
+        # respo = cloneHandler.handler(name, projectId)
+        response = "{\n"
+ 
+        info = saagie.API.projects.get_info(projectId)
+ 
+        response = response +  str(info['project'])
+        info = saagie.API.projects.get_rights(projectId)
+        response = response + str(info['rights']) + "{\n"
+ 
+        respo = saagie.API.jobs.list_for_project(project_id=projectId, instances_limit=2)
+        for job in respo['jobs']:
+            response = response +"{\n"
+            result= saagie.API.jobs.get_info(job_id=job['id'], instances_limit=2)
+            response = response + str(result)+ "\n}"
+ 
+        response = response +"\n}\n{"
+ 
+        respo = saagie.API.apps.list_for_project(project_id = projectId)
+        for app in respo['labWebApps']:
+ 
+            response = response +"{\n"+ str(app)+ "\n}"
+ 
+ 
+        response = response +"\n}\n{"
+ 
+        respo = saagie.API.pipelines.list_for_project(project_id= projectId)
+        for app in respo['project']['pipelines']:
+ 
+            response = response +"{\n"+ str(app)+ "\n}"
+ 
+        response = response +"\n}\n{"
+ 
+        respo = saagie.API.docker_credentials.list_for_project(project_id= projectId)
+        for app in respo['allDockerCredentials']:
+ 
+            response = response +"{\n"+ str(app)+ "\n}"
+ 
+    return response + "\n}"
 
-@app.route('/testjob/<string:idjob>')
-def jinfo(idjob):
-    
-    infojob = saagie.API.jobs.get_info(idjob)
-    return infojob
 
 
 if __name__ == "__main__":
